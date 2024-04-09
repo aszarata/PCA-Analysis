433f73605b97bc6379134144bb82635f8416e996
wiktorwalczak <wikwalczak@student.agh.edu.pl>
2024-04-09 09:06:47 +0200

    updated parameters -> kmeans & dbscan -> user can test grouping with any number he/she wants

--- gui/clustering_pca_dialog.py
+++ gui/clustering_pca_dialog.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from PyQt5.QtWidgets import QPushButton, QLabel, QHBoxLayout, QComboBox, QVBoxLayout, QDialog, QLineEdit, QMessageBox, \
-    QSpinBox, QWidget, QCheckBox, QFormLayout, QFileDialog
+    QSpinBox, QWidget, QCheckBox, QFormLayout, QFileDialog, QInputDialog
 from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -140,6 +140,83 @@ class PlotDialog(QDialog):
         self.canvas.draw()
 
 
+# class KMeansSuggestionDialog(QDialog):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         self.pca_handler = parent.pca_handler
+#         self.init_ui()
+#
+#     def init_ui(self):
+#         self.setWindowTitle('Grupowanie KMeans')
+#         self.setGeometry(100, 100, 300, 200)  # Dostosowany rozmiar okna
+#
+#         layout = QVBoxLayout()
+#         form_layout = QFormLayout()
+#
+#         self.max_clusters_input = QLineEdit()
+#         # self.draw_graph_checkbox = QCheckBox("Rysuj wykres")
+#
+#         form_layout.addRow("Maksymalna liczba klastrów:", self.max_clusters_input)
+#         # form_layout.addRow(self.draw_graph_checkbox)
+#
+#         self.run_button = QPushButton("Podaj optymalną liczbę klastrów")
+#         self.run_button.clicked.connect(self.run_suggestion)
+#
+#         layout.addLayout(form_layout)
+#         layout.addWidget(self.run_button)
+#         self.setLayout(layout)
+#
+#     def run_suggestion(self):
+#         max_clusters = self.max_clusters_input.text()
+#         # draw_graph = self.draw_graph_checkbox.isChecked()
+#
+#         # Walidacja wejścia użytkownika
+#         if not max_clusters.isdigit() or int(max_clusters) < 2:
+#             QMessageBox.warning(self, "Błąd", "Maksymalna liczba klastrów musi być liczbą większą niż 1.")
+#             return
+#
+#         max_clusters = int(max_clusters)
+#
+#         try:
+#             # Zakładamy, że data_instance posiada metodę suggest_clusters_kmeans
+#             optimal_clusters = self.pca_handler.suggest_clusters_kmeans(max_clusters=max_clusters,
+#                                                                         draw_graph=False)
+#             QMessageBox.information(self, "Sugerowana liczba klastrów", f"Optymalna liczba klastrów: {optimal_clusters}")
+#
+#             # if draw_graph:
+#             #     # Zakładamy, że metoda suggest_clusters_kmeans rysuje wykres, gdy draw_graph=True
+#             #     # Jeśli metoda nie rysuje wykresu bezpośrednio, można dodać logikę rysowania tutaj.
+#             #     pass
+#             # Kod do obliczenia optymalnej liczby klastrów...
+#             user_decision = QMessageBox.question(self, "Uruchom KMeans",
+#                                                  "Czy chcesz uruchomić klastrowanie KMeans z optymalną liczbą klastrów?",
+#                                                  QMessageBox.Yes | QMessageBox.No)
+#
+#             if user_decision == QMessageBox.Yes:
+#                 self.run_kmeans(optimal_clusters)  # Uruchamiamy KMeans z zasugerowaną liczbą klastrów
+#
+#         except Exception as e:
+#             QMessageBox.critical(self, "Błąd",
+#                                  f"Wystąpił błąd podczas obliczania sugerowanej liczby klastrów.")
+#
+#     def run_kmeans(self, optimal_clusters):
+#         try:
+#             self.parent().data_instance.save()
+#             self.pca_handler.kmeans_clustering(num_clusters=optimal_clusters)
+#             self.parent().kmeans_done = True
+#             QMessageBox.information(self, "KMeans Clustering",
+#                                     f"Klastrowanie KMeans zakończone dla {optimal_clusters} klastrów.")
+#
+#             # Utworzenie instancji okna dialogowego i wyświetlenie wykresu
+#             plot_dialog = PlotDialog(self)
+#             plot_dialog.plot(self.pca_handler, 'pc1', 'pc2', f'Klastry KMeans dla {optimal_clusters} klastrów')
+#             plot_dialog.exec_()
+#
+#         except ValueError as e:
+#             QMessageBox.critical(self, "Wystąpił błąd - spróbuj ponownie")
+#         except Exception as e:
+#             QMessageBox.critical(self, "Wystąpił błąd", f"Wystąpił nieoczekiwany błąd.")
+
 class KMeansSuggestionDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -168,9 +245,6 @@ class KMeansSuggestionDialog(QDialog):
 
     def run_suggestion(self):
         max_clusters = self.max_clusters_input.text()
-        # draw_graph = self.draw_graph_checkbox.isChecked()
-
-        # Walidacja wejścia użytkownika
         if not max_clusters.isdigit() or int(max_clusters) < 2:
             QMessageBox.warning(self, "Błąd", "Maksymalna liczba klastrów musi być liczbą większą niż 1.")
             return
@@ -178,27 +252,23 @@ class KMeansSuggestionDialog(QDialog):
         max_clusters = int(max_clusters)
 
         try:
-            # Zakładamy, że data_instance posiada metodę suggest_clusters_kmeans
-            optimal_clusters = self.pca_handler.suggest_clusters_kmeans(max_clusters=max_clusters,
-                                                                        draw_graph=False)
-            QMessageBox.information(self, "Sugerowana liczba klastrów", f"Optymalna liczba klastrów: {optimal_clusters}")
+            optimal_clusters = self.pca_handler.suggest_clusters_kmeans(max_clusters=max_clusters, draw_graph=False)
+            suggested_clusters, okPressed = QInputDialog.getInt(
+                self, "Sugerowana liczba klastrów", "Optymalna liczba klastrów:", optimal_clusters, 2, max_clusters, 1)
 
-            # if draw_graph:
-            #     # Zakładamy, że metoda suggest_clusters_kmeans rysuje wykres, gdy draw_graph=True
-            #     # Jeśli metoda nie rysuje wykresu bezpośrednio, można dodać logikę rysowania tutaj.
-            #     pass
-            # Kod do obliczenia optymalnej liczby klastrów...
-            user_decision = QMessageBox.question(self, "Uruchom KMeans",
-                                                 "Czy chcesz uruchomić klastrowanie KMeans z optymalną liczbą klastrów?",
-                                                 QMessageBox.Yes | QMessageBox.No)
+            if okPressed:
+                user_decision = QMessageBox.question(self, "Uruchom KMeans",
+                                                     "Czy chcesz uruchomić klastrowanie KMeans z wybraną liczbą klastrów?",
+                                                     QMessageBox.Yes | QMessageBox.No)
 
-            if user_decision == QMessageBox.Yes:
-                self.run_kmeans(optimal_clusters)  # Uruchamiamy KMeans z zasugerowaną liczbą klastrów
+                if user_decision == QMessageBox.Yes:
+                    self.run_kmeans(
+                        suggested_clusters)  # Uruchamiamy KMeans z zasugerowaną/zmodyfikowaną liczbą klastrów
+            else:
+                QMessageBox.information(self, "Anulowano", "Operacja została anulowana przez użytkownika.")
 
         except Exception as e:
-            QMessageBox.critical(self, "Błąd",
-                                 f"Wystąpił błąd podczas obliczania sugerowanej liczby klastrów.")
-
+            QMessageBox.critical(self, "Błąd", "Wystąpił błąd podczas obliczania sugerowanej liczby klastrów.")
     def run_kmeans(self, optimal_clusters):
         try:
             self.parent().data_instance.save()
@@ -218,27 +288,93 @@ class KMeansSuggestionDialog(QDialog):
             QMessageBox.critical(self, "Wystąpił błąd", f"Wystąpił nieoczekiwany błąd.")
 
 
+# class DBSCANDialog(QDialog):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         self.pca_handler = parent.pca_handler  # Zakładamy, że PCA Handler jest dostępny w rodzicu
+#         self.init_ui()
+#
+#     def init_ui(self):
+#         self.setWindowTitle('Grupowanie DBSCAN')
+#         self.setGeometry(100, 100, 300, 200)
+#
+#         layout = QVBoxLayout()
+#         self.setLayout(layout)
+#
+#         form_layout = QFormLayout()
+#         self.min_samples_input = QLineEdit()
+#         form_layout.addRow("Min samples -> minimalna liczba punktów, które muszą się znaleźć w klastrze", self.min_samples_input)
+#
+#         self.run_button = QPushButton("Zasugeruj")
+#         self.run_button.clicked.connect(self.run_dbscan_suggestion)
+#         layout.addLayout(form_layout)
+#         layout.addWidget(self.run_button)
+#
+#     def run_dbscan_suggestion(self):
+#         min_samples = self.min_samples_input.text()
+#         if not min_samples.isdigit() or int(min_samples) <= 0:
+#             QMessageBox.warning(self, "Błąd", "Min samples musi być liczbą większą niż 0.")
+#             return
+#
+#         min_samples = int(min_samples)
+#         try:
+#             optimal_eps = self.pca_handler.suggest_clusters_dbscan(min_samples=min_samples, draw_graph=False)
+#             QMessageBox.information(self, "Sugerowane EPS", f"Zasugerowana wartość optimal eps: {optimal_eps}")
+#
+#             self.parent().data_instance.save()
+#             self.pca_handler.dbscan_clustering(eps=optimal_eps, min_samples=min_samples)
+#             self.parent().dbscan_done = True
+#             self.display_dbscan_results()
+#
+#         except Exception as e:
+#             QMessageBox.critical(self, "Wystąpił błąd", f"Wystąpił błąd - spróbuj ponownie")
+#
+#     def display_dbscan_results(self):
+#         plot_dialog = PlotDialog(self)
+#         plot_dialog.plot(self.pca_handler, 'pc1', 'pc2', 'DBSCAN Clusters')
+#         plot_dialog.exec_()
+
 class DBSCANDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.pca_handler = parent.pca_handler  # Zakładamy, że PCA Handler jest dostępny w rodzicu
+        self.optimal_eps = None  # Miejsce na przechowywanie sugerowanego eps
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle('Grupowanie DBSCAN')
-        self.setGeometry(100, 100, 300, 200)
+        self.setGeometry(100, 100, 300, 250)  # Zwiększenie wysokości okna
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
         form_layout = QFormLayout()
         self.min_samples_input = QLineEdit()
-        form_layout.addRow("Min samples:", self.min_samples_input)
+        self.eps_input = QLineEdit()  # Pole do wprowadzenia i edycji wartości eps
+        self.eps_input.setEnabled(False)  # Na początku pole jest wyłączone
+
+        self.min_samples_input.textChanged.connect(self.enable_suggestion)  # Włączenie ponownego sugerowania
+        self.eps_input.setMinimumWidth(400)  # Ustawienie minimalnej szerokości pola eps_input
+
+        form_layout.addRow("Min samples -> minimalna liczba punktów, które muszą się znaleźć w klastrze:", self.min_samples_input)
+        form_layout.addRow("Optimal eps -> sugerowana wartość eps (możesz edytować):", self.eps_input)
 
         self.run_button = QPushButton("Zasugeruj")
         self.run_button.clicked.connect(self.run_dbscan_suggestion)
+
+        self.apply_button = QPushButton("Zastosuj EPS i uruchom DBSCAN")
+        self.apply_button.clicked.connect(self.apply_dbscan)
+        self.apply_button.setEnabled(False)  # Na początku przycisk jest wyłączony
+
         layout.addLayout(form_layout)
         layout.addWidget(self.run_button)
+        layout.addWidget(self.apply_button)
+
+    def enable_suggestion(self):
+        self.eps_input.clear()
+        self.eps_input.setEnabled(False)
+        self.apply_button.setEnabled(False)
+        self.run_button.setEnabled(True)
 
     def run_dbscan_suggestion(self):
         min_samples = self.min_samples_input.text()
@@ -248,16 +384,26 @@ class DBSCANDialog(QDialog):
 
         min_samples = int(min_samples)
         try:
-            optimal_eps = self.pca_handler.suggest_clusters_dbscan(min_samples=min_samples, draw_graph=False)
-            QMessageBox.information(self, "Sugerowane EPS", f"Zasugerowana wartość optimal eps: {optimal_eps}")
-
-            self.parent().data_instance.save()
-            self.pca_handler.dbscan_clustering(eps=optimal_eps, min_samples=min_samples)
-            self.parent().dbscan_done = True
-            self.display_dbscan_results()
-
+            self.optimal_eps = self.pca_handler.suggest_clusters_dbscan(min_samples=min_samples, draw_graph=False)
+            self.eps_input.setText(str(self.optimal_eps))  # Ustawienie sugerowanej wartości w polu
+            self.eps_input.setEnabled(True)  # Włączenie pola do edycji
+            self.apply_button.setEnabled(True)  # Włączenie przycisku zastosuj
         except Exception as e:
-            QMessageBox.critical(self, "Wystąpił błąd", f"Wystąpił błąd - spróbuj ponownie")
+            QMessageBox.critical(self, "Wystąpił błąd", "Wystąpił błąd - spróbuj ponownie")
+
+    def apply_dbscan(self):
+        # Zastosowanie wartości eps z pola eps_input
+        eps = self.eps_input.text()
+        try:
+            eps = float(eps)  # Konwersja na float, jeśli to możliwe
+        except ValueError:
+            QMessageBox.warning(self, "Błąd", "Wartość EPS musi być liczbą.")
+            return
+
+        min_samples = int(self.min_samples_input.text())
+        self.pca_handler.dbscan_clustering(eps=eps, min_samples=min_samples)
+        self.parent().dbscan_done = True
+        self.display_dbscan_results()
 
     def display_dbscan_results(self):
         plot_dialog = PlotDialog(self)
